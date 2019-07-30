653558db47c3718c74e623169149662f0097f18d
Greg Doud <gpdoud@gmail.com>
2019-07-30 11:14:43 -0400

    Completed

---
+++ OopBankingProject/Checking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopBankingProject {
+
+    public class Checking : Account {
+
+        private int lastCheckNumber { get; set; } = 0;
+
+        public bool Pay(int CheckNumber, decimal Amount) {
+
+            bool success = this.Withdraw(Amount);
+            if(success) {
+                this.lastCheckNumber = CheckNumber;
+                return true;
+            }
+            return false;
+        }
+
+        public override string Print() {
+            return base.Print() + $" | {lastCheckNumber}";
+        }
+
+    }
+}
--- OopBankingProject/Program.cs
+++ OopBankingProject/Program.cs
@@ -18,52 +18,57 @@ namespace OopBankingProject {
             mm.MMRate = 0.05;
             mm.PayInterest(12);
 
-            Account[] accounts = new Account[] { sav1, mm };
+            Checking chk = new Checking();
+            chk.Number = "CHK001";
+            chk.Name = "My checking account";
+            chk.Deposit(100);
+            chk.Pay(100, 20);
+
+            Account[] accounts = new Account[] { sav1, mm, chk };
 
             foreach(Account acct in accounts) {
                 Console.WriteLine(acct.Print());
             }
 
-            Console.WriteLine(sav1.Print());
-            Console.WriteLine(mm.Print());
-            /*
-                                    bool ItWorked = mm.TransferTo(sav1, 150);
-                                    Console.WriteLine($"Money Market balance is {mm.GetBalance()}");
-                                    Console.WriteLine($"Saving1 balance is {sav1.GetBalance()}");
-                                    /*
-                                                            Savings sav = new Savings();
-                                                            sav.Number = "1002";
-                                                            sav.Name = "Savings 1";
-                                                            sav.IntRate = 0.1;
 
-                                                            sav.Deposit(200);
-                                                            sav.Withdraw(100);
-                                                            decimal interestToBePaid = sav.CalcInterest(12);
-                                                            sav.PayInterest(interestToBePaid);
-                                                            decimal savbal = sav.GetBalance();
-                                                            Console.WriteLine($"Savings balance is {savbal}");
-                                                /*
-                                                            Account acct = new Account();
-                                                            acct.Number = "1001";
-                                                            acct.Name = "Test Account";
+/*
+            bool ItWorked = mm.TransferTo(sav1, 150);
+            Console.WriteLine($"Money Market balance is {mm.GetBalance()}");
+            Console.WriteLine($"Saving1 balance is {sav1.GetBalance()}");
 
-                                                            acct.Deposit(20);
-                                                            acct.Withdraw(10);
-                                                            decimal balance = acct.GetBalance();
-                                                            Console.WriteLine($"Account balance is {balance} (should be 10)");
+            Savings sav = new Savings();
+            sav.Number = "1002";
+            sav.Name = "Savings 1";
+            sav.IntRate = 0.1;
 
-                                                            acct.Deposit(-10);
-                                                            balance = acct.GetBalance();
-                                                            Console.WriteLine($"Account balance is {balance} (should be 10)");
+            sav.Deposit(200);
+            sav.Withdraw(100);
+            decimal interestToBePaid = sav.CalcInterest(12);
+            sav.PayInterest(interestToBePaid);
+            decimal savbal = sav.GetBalance();
+            Console.WriteLine($"Savings balance is {savbal}");
+/*
+            Account acct = new Account();
+            acct.Number = "1001";
+            acct.Name = "Test Account";
 
-                                                            acct.Withdraw(-10);
-                                                            balance = acct.GetBalance();
-                                                            Console.WriteLine($"Account balance is {balance} (should be 10)");
+            acct.Deposit(20);
+            acct.Withdraw(10);
+            decimal balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (should be 10)");
 
-                                                            acct.Withdraw(20);
-                                                            balance = acct.GetBalance();
-                                                            Console.WriteLine($"Account balance is {balance} (should be 10)");
-                                                */
+            acct.Deposit(-10);
+            balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (should be 10)");
+
+            acct.Withdraw(-10);
+            balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (should be 10)");
+
+            acct.Withdraw(20);
+            balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (should be 10)");
+*/
         }
     }
 }
